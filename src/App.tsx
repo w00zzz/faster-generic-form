9d378bb340b586e5ff114669df4dbc98266e1e52
robert <w00zzz@proton.me>
2025-09-16 17:32:24 -0400

    se simplifico el app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import GenericForm from './components/GenericForm/component.form/form.generic'
 import { Box, Button, ThemeProvider } from '@mui/material'
@@ -8,7 +6,6 @@ import theme from './theme'
 import type { ITextField } from './components/GenericForm/types/controls.types'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [showForm, setShowForm] = useState(false)
 
   const formControls: ITextField[] = [
@@ -42,23 +39,6 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
       
       {/* Ejemplo de GenericForm */}
       <Box sx={{ mt: 4, p: 2 }}>
@@ -81,9 +61,6 @@ function App() {
         )}
       </Box>
       
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </ThemeProvider>
   )
 }
